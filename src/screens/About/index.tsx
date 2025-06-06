27177586da580a7188483510f0c53d086b6c28cf
Efrain Marcelo Pulgar Pantaleon <efrainmpp@hotmail.com>
2025-06-06 16:55:38 -0300

    Add BackHomeButton and align home navigation buttons

--- src/screens/About/index.tsx
+++ src/screens/About/index.tsx
@@ -1,25 +1,13 @@
 import React from "react";
-import { Box, Button, Container } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-import { Home } from "lucide-react";
+import { Box } from "@mui/material";
 import AboutSection from "../Home/AboutSection";
+import BackHomeButton from "../../libs/components/BackHomeButton";
 
 const AboutPage: React.FC = () => {
-  const navigate = useNavigate();
 
   return (
     <Box sx={{ pt: { xs: 8, md: 10 } }}>
-      <Container maxWidth="lg" sx={{ py: 2 }}>
-        <Button
-          startIcon={<Home />}
-          onClick={() => navigate("/")}
-          variant="outlined"
-          sx={{ mb: 4 }}
-        >
-          Voltar para Home
-        </Button>
-      </Container>
-      <AboutSection />
+      <AboutSection topAction={<BackHomeButton />} />
     </Box>
   );
 };
