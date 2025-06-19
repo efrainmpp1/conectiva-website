df410d986fd7a52dad5d7ec0d435f08c9a291b08
Efrain Marcelo Pulgar Pantaleon <efrainmpp@hotmail.com>
2025-06-19 13:28:28 -0300

    feat: add prospeccao inteligente page inside dashboard

---
+++ src/screens/ProspeccaoInteligente/ProspeccaoInteligente.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+import ConstructionIcon from "@mui/icons-material/Construction";
+import BackDashboardButton from "../../libs/components/BackDashboardButton";
+
+const ProspeccaoInteligentePage: React.FC = () => {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      height="80vh"
+      textAlign="center"
+    >
+      <ConstructionIcon sx={{ fontSize: 80, mb: 2, color: "primary.main" }} />
+      <Typography variant="h3" gutterBottom>
+        Funcionalidade em Desenvolvimento 🚧
+      </Typography>
+      <Typography variant="body1" mb={4}>
+        Estamos trabalhando nesta funcionalidade. Em breve, ela estará disponível para você!
+      </Typography>
+      <BackDashboardButton />
+    </Box>
+  );
+};
+
+export default ProspeccaoInteligentePage;
