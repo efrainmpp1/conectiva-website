f2f9fb6255542ee77a70491df4b501cd18980b08
efrainmpp1 <efrainmpp@hotmail.com>
2025-07-01 17:27:15 -0300

    fix response of route register route

--- src/services/users/integration.ts
+++ src/services/users/integration.ts
@@ -2,6 +2,6 @@ import { apiNode } from '../apiNode';
 import { RegisterNewUser, User } from '../../libs/interfaces/User';
 
 export const registerUser = async (newUserRegisterData: RegisterNewUser): Promise<User> => {
-  const { data } = await apiNode.post<{ newUser: User }>('/users', newUserRegisterData);
-  return data.newUser;
+  const { data } = await apiNode.post<{ user: User }>('/users', newUserRegisterData);
+  return data.user;
 };
