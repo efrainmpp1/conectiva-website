9dc359eefc3858e42c2487f04e59bc98f9202ac9
efrainmpp1 <efrainmpp@hotmail.com>
2025-07-03 19:13:39 -0300

    fix: add rest of getUserHistoricalServices metod

--- src/services/users/integration.ts
+++ src/services/users/integration.ts
@@ -1,5 +1,6 @@
 import { apiNode } from '../apiNode';
 import { RegisterNewUser, User } from '../../libs/interfaces/User';
+import { UserHistoricalService } from '../../libs/interfaces/UserHistoricalService';
 
 export const registerUser = async (newUserRegisterData: RegisterNewUser): Promise<User> => {
   const { data } = await apiNode.post<{ user: User }>('/users', newUserRegisterData);
@@ -9,4 +10,11 @@ export const registerUser = async (newUserRegisterData: RegisterNewUser): Promis
 export const getUserByFirebaseUid = async (firebaseUid: string): Promise<User> => {
   const { data } = await apiNode.get<{ user: User }>(`/users/${firebaseUid}`);
   return data.user;
+};
+
+export const getUserHistoricalServices = async (id: number): Promise<UserHistoricalService[]> => {
+  const { data } = await apiNode.get<{ historicalServices: UserHistoricalService[] }>(
+    `/users/${id}/historical-services`,
+  );
+  return data.historicalServices;
 };
